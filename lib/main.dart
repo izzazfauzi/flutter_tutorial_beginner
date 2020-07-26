03cf8fd2ff70489c16e9a58e985db89d8efcfc06
izzazfauzi <m.izzaz93@gmail.com>
2020-07-27 00:23:11 +0800

    Colors & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,22 @@ void main() {
         appBar: AppBar(
           title: Text('Ezaz first app'),
           centerTitle: true,
+          backgroundColor: Colors.teal[900],
         ),
         body: Center(
-          child: Text('Hi Ezaz!'),
+          child: Text(
+            'Hi Ezaz!',
+            style: TextStyle(
+              fontFamily: 'FredokaOne',
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.blueGrey[600],
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.teal[900],
           child: Text('click'),
           onPressed: null,
         ),
