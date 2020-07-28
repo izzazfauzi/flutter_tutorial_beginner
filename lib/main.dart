720ee2b0580d52f3606e1667d24708a2222e5b49
izzazfauzi <m.izzaz93@gmail.com>
2020-07-28 15:59:57 +0800

    Ninja ID Project

--- lib/main.dart
+++ lib/main.dart
@@ -12,47 +12,88 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal[900],
       appBar: AppBar(
-        title: Text('Ezaz first app'),
+        title: Text('Ezaz ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.teal[900],
+        backgroundColor: Colors.teal[800],
+        elevation: 0.0,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Image.asset('assets/background2.jpg'),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.cyan,
-              child: Text('1'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/profile.png'),
+                radius: 50.0,
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.amber,
-              child: Text('2'),
+            Divider(
+              height: 60.0,
+              color: Colors.grey,
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.pinkAccent,
-              child: Text('3'),
+            Text(
+              'NAME',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.teal[900],
-        child: Text('click'),
-        onPressed: () {},
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Muhammad Izzaz bin Fauzi',
+              style: TextStyle(
+                  color: Colors.white70, letterSpacing: 2.0, fontSize: 20.0),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'AGE',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '27',
+              style: TextStyle(
+                color: Colors.white70,
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'm.izzaz93@gmail.com',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    letterSpacing: 2.0,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
