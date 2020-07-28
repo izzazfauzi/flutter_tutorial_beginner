ff0f9d2b51e1f391f1c9b6c35dfb49c21a04e694
izzazfauzi <m.izzaz93@gmail.com>
2020-07-28 17:22:37 +0800

    Custom Classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(
@@ -14,10 +15,10 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple',
+  List<Quote> quotes = [
+    Quote(author: 'ezaz', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'scha', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'rora', text: 'The truth is rarely pure and never simple'),
   ];
 
   @override
@@ -30,7 +31,9 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.deepPurple[800],
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes
+            .map((quote) => Text('${quote.text} - ${quote.author}'))
+            .toList(),
       ),
     );
   }
