a4c8eb7a11172554a413a2d2fc1670cd6738851d
izzazfauzi <m.izzaz93@gmail.com>
2020-07-28 18:36:14 +0800

    Cards

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,38 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'rora', text: 'The truth is rarely pure and never simple'),
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blueGrey[600],
+              ),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.blueGrey[800],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,9 +63,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.deepPurple[800],
       ),
       body: Column(
-        children: quotes
-            .map((quote) => Text('${quote.text} - ${quote.author}'))
-            .toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
