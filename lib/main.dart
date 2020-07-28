7b454938e889bb22f1cd8744ccd6ed901f380d42
izzazfauzi <m.izzaz93@gmail.com>
2020-07-28 16:47:20 +0800

    Lists of Data

--- lib/main.dart
+++ lib/main.dart
@@ -3,113 +3,34 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Home(),
+      home: QuoteList(),
     ),
   );
 }
 
-class Home extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
-  int level = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal[900],
+      backgroundColor: Colors.deepPurple[900],
       appBar: AppBar(
-        title: Text('Ezaz ID Card'),
+        title: Text('Awesome Quote'),
         centerTitle: true,
-        backgroundColor: Colors.teal[800],
-        elevation: 0.0,
+        backgroundColor: Colors.deepPurple[800],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.teal[800],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/profile.png'),
-                radius: 50.0,
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Muhammad Izzaz bin Fauzi',
-              style: TextStyle(
-                  color: Colors.white70, letterSpacing: 2.0, fontSize: 20.0),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'LEVEL',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '$level',
-              style: TextStyle(
-                color: Colors.white70,
-                letterSpacing: 2.0,
-                fontSize: 20.0,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'm.izzaz93@gmail.com',
-                  style: TextStyle(
-                    color: Colors.white70,
-                    letterSpacing: 2.0,
-                    fontSize: 20.0,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
       ),
     );
   }
